86f4e39d2e64df9ea7a4bdff7d9498f86271cbe7
aray-1 <humanrace.450@gmail.com>
2023-03-28 19:25:02 +0530

    mongoDB URI

--- server.js
+++ server.js
@@ -10,20 +10,16 @@ const payment_routes = require("./routes/paymentRoutes");
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/BankData', { useNewUrlParser: true });
-const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to database'));
 
 // connecting to mongoDB Atlas server
-// mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-// .then((result)=>{
-//     app.listen(4000);
-//     console.log("Server Running On Port 4000");
-// })
-// .catch((err)=>{
-//     console.log(err);
-// })
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then((result)=>{
+    app.listen(3001);
+    console.log("Server Running On Port 4000");
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -31,6 +27,3 @@ app.use(cors());
 app.use("/customers", customer_routes);
 app.use("/accounts", account_routes);
 app.use("/payment", payment_routes);
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
